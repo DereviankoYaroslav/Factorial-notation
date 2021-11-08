768d3a516ece13505932e6f2f703dc30b5886dd2
olenaderevianko <dereviankoolenav@gmail.com>
2021-11-08 13:43:36 +0200

    +implementation from document (number/2,3,4...)

--- main.c
+++ main.c
@@ -4,19 +4,26 @@
 
 int *numberToFactorial(int x, int n);
 int *numberToSubstitution(int *number, int size);
-void substitutionToFactorial(int *sub, int size);
+int *substitutionToFactorial(int *sub, int size);
+int factorialCounting(int x);
 
 int main(){
 
     int n = 5;
     int *arr = numberToFactorial(100,n);
     int *sub = numberToSubstitution(arr,n);
-    printf("\n\nRESULT\n");
+    printf("\n\nSUBSTITUTION\n");
     for (int i = 0; i < n; ++i){
         printf("%d, ",sub[i]);
     }
+    int *factNum = substitutionToFactorial(sub,n);
+    printf("\n\nFACTORIAL NUMBER\n");
+    for (int j = 0; j < n; ++j){
+        printf("%d, ",factNum[j]);
+    }
     printf("\n");
-    substitutionToFactorial(sub,n);
+    int a = factorialCounting(10);
+    printf("%d ",a);
 
 }
 
@@ -36,23 +43,23 @@ int *numberToSubstitution(int *number, int size){
             }
         }
         for (int k =0; k <newSize; ++k){
-            printf("%d, ",emptyPos[k]);
+            //printf("%d, ",emptyPos[k]);
         }
         for (int q = 0; q < newSize; ++q){
             if (q==number[newSize-1]){
-                printf("q = %d ",q);
-                printf("MT pos = %d ",emptyPos[q]);
+                //printf("q = %d ",q);
+                //printf("MT pos = %d ",emptyPos[q]);
                 S[emptyPos[q]] = 1;
                 result[coeffNum] = emptyPos[q];
                 ++coeffNum;
             }
         }
         innerCounter = 0;
-        printf("\n");
+        //printf("\n");
         for (int k =0; k <size; ++k){
-            printf("%d, ",S[k]);
+            //printf("%d, ",S[k]);
         }
-        printf("\n");
+        //printf("\n");
         ++counter;
         newSize--;
         free(emptyPos);
@@ -74,9 +81,10 @@ int *numberToSubstitution(int *number, int size){
 }
 
 int *numberToFactorial(int x, int n){
-    int q = 1;
-    int counter = n-1;
+    int q = 2;
+    int counter = n-2;
     int *positions = calloc (n,sizeof(int));
+    positions[n-1] = 0;
     while (counter > 0){
         int temp = floor(x/q);
         printf("\nq = %d ",q);
@@ -89,20 +97,25 @@ int *numberToFactorial(int x, int n){
         counter--;
     }
     positions[counter] = x;
+    printf("\nPOSITIONS VECTOR\n");
+    for (int i = 0; i < n ; ++i){
+        printf("%d ",positions[i]);
+    }
     int *positionsRev = calloc (n,sizeof(int));
     for (int r = 0,t = n-1; r < n, t>=0; ++r, t--){
         positionsRev[t] = positions[r];
     }
-    printf("\n\nPOSITIONS VECTOR\n");
+    //printf("\n\nPOSITIONS VECTOR\n");
     for (int i = 0; i < n ; ++i){
-        printf("%d ",positionsRev[i]);
+        //printf("%d ",positionsRev[i]);
     }
-    printf("\n");
+    //printf("\n");
     free(positions);
     return positionsRev;
 }
 
-void substitutionToFactorial(int *sub, int size) {
+int *substitutionToFactorial(int *sub, int size) {
+    int *result = calloc(size,sizeof(int));
     int value = size;
     int flag = 0;
     int innerCounter = 0;
@@ -116,10 +129,16 @@ void substitutionToFactorial(int *sub, int size) {
                 flag = 1;
             }
         }
-        printf("IC = %d ",innerCounter);
+        //printf("IC = %d ",innerCounter);
+        result[counter] = innerCounter;
         innerCounter = 0;
         flag = 0;
         ++counter;
         value--;
     }
+    return result;
+}
+
+int factorialCounting(int x){
+    return (x==0) || (x==1) ? 1 : x* factorialCounting(x-1);
 }
